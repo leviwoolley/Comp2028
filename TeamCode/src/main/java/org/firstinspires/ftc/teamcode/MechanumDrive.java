e1f05644d715eb1bbb27d13b89a3348a58ea1c6f
Levi Woolley <leviwoolley0@gmail.com>
2026-09-18 11:05:48 -0600

    Added (working?) MechanumDrive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumDrive.java
@@ -8,21 +8,72 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 @TeleOp(name="Mechanum Drive", group="Linear OpMode")
-@Disabled
+
 public class MechanumDrive extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
-    private ElapsedTime runtime = new ElapsedTime();
+    
     private DcMotor fL;
     private DcMotor bL;
     private DcMotor fR;
     private DcMotor bR;
 
+    private double x;
+    private double y;
+    private double turn;
+    private double theta;
+
+    private double frontLeftP;
+    private double backLeftP;
+    private double frontRightP;
+    private double backRightP;
+
     @Override
     public void runOpMode() {
 
+        fL = hardwareMap.get(DcMotor.class, "fL");
+        fR = hardwareMap.get(DcMotor.class, "fR");
+        bL = hardwareMap.get(DcMotor.class, "bL");
+        bR = hardwareMap.get(DcMotor.class, "bR");
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            
+
+            x = gamepad1.left_stick_x;
+            y = -gamepad1.left_stick_y;
+            turn = gamepad1.right_stick_x;
+
+            theta = Math.atan2(y, x);
+            power = Math.hypot(x, y);
+
+            sin = Math.sin(theta - Math.PI/4);
+            cos = Math.cos(theta - Math.PI/4);
+            max = Math.max(Math.abs(sin), Math.abs(cos));
+
+            frontLeftP = power * cos/max + turn;
+            frontRightP = power * sin/max - turn;
+            backLeftP = power * sin/max + turn;
+            backRightP = power * cos/max - turn;
+
+            if ((power + Math.abs(turn)) > 1) {
+                frontLeftP /= power + Math.abs(turn);
+                frontRightP /= power + Math.abs(turn);
+                backLeftP /= power + Math.abs(turn);
+                backRightP = power + Math.abs(turn);
+            }
+
+            fL.setPower(frontLeftP);
+            fR.setPower(frontRightP);
+            bL.setPower(backLeftP);
+            bR.setPower(backRightP);
+
+            telemetry.addData("Front left/Right", "%4.2f, %4.2f", frontLeftP, frontRightP);
+            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", backLeftP, backRightP);
+            telemetry.update();
+
         }
     }}
